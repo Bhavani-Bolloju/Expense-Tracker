17e14974ad8d64c6c6a4f179f4e93372c01b797c
Bhavani Bolloju <bhavani.bolloju@gmail.com>
2026-03-09 17:00:54 +0530

    update notification options

--- client/src/UI/notification.js
+++ client/src/UI/notification.js
@@ -2,7 +2,15 @@ import { Notyf } from "notyf";
 
 import "notyf/notyf.min.css";
 
-const notyf = new Notyf();
+const notyf = new Notyf({
+  duration: 1000,
+  ripple: true,
+  position: {
+    x: "right",
+    y: "top"
+  },
+  dismissible: true
+});
 
 export default notyf;
 
