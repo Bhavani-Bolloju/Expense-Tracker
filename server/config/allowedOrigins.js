92117f75cd457aa92e7559c8624ef2b05b7b954b
Bhavani Bolloju <bhavani.bolloju@gmail.com>
2026-03-13 17:08:08 +0530

    update allowed origins list with deployed client link

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,4 +1,7 @@
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://keen-chimera-661bf9.netlify.app/"
+];
 
 module.exports = allowedOrigins;
 
